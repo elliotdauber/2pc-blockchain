17302d947cbfad478ce6e35d4a6073dc3d0e6865
Elliot Dauber <elliotkdauber@gmail.com>
2022-04-14 01:43:59 -0700

    minor

--- contracts/TPC.sol
+++ contracts/TPC.sol
@@ -9,10 +9,10 @@ pragma solidity >=0.7.0 <0.9.0;
 contract TPC {
 
     enum State{ INIT, VOTING, COMMIT, ABORT }
-    uint256[] _participants;
-    uint256[] _voted;
-    uint256 _timeout;
-    State _state = State.INIT;
+    uint256[] public _participants;
+    uint256[] public _voted;
+    uint256 public _timeout;
+    State public _state = State.INIT;
 
     function request(uint256[] memory participants, uint256 timeout) public {
         assert(_state == State.INIT);
