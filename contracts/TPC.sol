1a52ea346f8c21314a527facf54b2b445cad3f66
Elliot Dauber <elliotkdauber@gmail.com>
2022-04-14 01:25:44 -0700

    added basic solidity file

---
+++ contracts/TPC.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+/**
+ * @title Storage
+ * @dev Store & retrieve value in a variable
+ */
+contract TPC {
+
+    enum State{ INIT, VOTING, COMMIT, ABORT }
+    uint256[] _participants;
+    uint256[] _voted;
+    uint256 _timeout;
+    State _state = State.INIT;
+
+    function request(uint256[] memory participants, uint256 timeout) public {
+        assert(_state == State.INIT);
+        _participants = participants;
+        _timeout = timeout;
+    }
+}
